7515cf0e72971dbd9b35b3e300de6938101913a7
crisliavh-10i <crisliavholnda@gmail.com>
2022-01-13 20:52:12 -0300

    update no css das paginas

--- contas/forms.py
+++ contas/forms.py
@@ -1,7 +1,15 @@
-from django.forms import ModelForm
+from django import forms
 from .models import Transacao
 
-class TransacaoForm(ModelForm):
+class TransacaoForm(forms.ModelForm):
     class Meta:
         model = Transacao
         fields = ['data', 'descriacao', 'valor', 'observacoes', 'categoria']
+
+        widgets = {
+            'data': forms.DateInput(attrs={'class':'form-control','display':'block' }),
+            'descriacao': forms.TextInput(attrs={'class':'form-control', }),
+            'valor': forms.NumberInput(attrs={'class':'form-control', }),
+            'observacoes': forms.Textarea(attrs={'class':'form-control', }),
+            'categoria': forms.Select(attrs={'class':'form-control', }),
+        }
